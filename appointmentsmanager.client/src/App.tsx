8891b367b45033e726a7c8e8efaad1ea49fb0462
micro <microfullstack@outlook.com>
2024-08-23 11:57:10 -0700

    code

--- appointmentsmanager.client/src/App.tsx
+++ appointmentsmanager.client/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css';
 import { Navbar } from "./components/MainNavBar/MainNavBar"
 import Page1 from "./components/page1/page1"
+import Page2 from './components/page2/page2'
 import { Route, Routes } from "react-router-dom";
 import { MainHeader } from "./components/MainHeader/MainHeader"
 import { DataProvider } from './common/DataContextProvider'
@@ -13,6 +14,7 @@ function App() {
             <Navbar></Navbar>
             <Routes>
                 <Route path="/page1" element={<Page1 />} />
+                <Route path="/page2" element={<Page2 />} />
             </Routes>
             </div>
         </DataProvider>
